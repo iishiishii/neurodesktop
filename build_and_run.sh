dafbe3937ff48c8980d2c574e5b7a8fb5b94b3c4
“stebo85@web.de” <opc@neurodesk.subnet09021545.vcn09021545.oraclevcn.com>
2022-03-29 04:46:12 +0000

    changed to podman and tried to fix interactive question in nextcluod install

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,4 +1,4 @@
 bash stop_and_clean.sh
-sudo docker build -t neurodesktop:latest .
-sudo docker run --shm-size=1gb -it --privileged --name neurodesktop -v ~/neurodesktop-storage:/neurodesktop-storage -e HOST_UID="$(id -u)" -e HOST_GID="$(id -g)" -p 8080:8080 neurodesktop:latest --debug
+sudo podman build -t neurodesktop:latest .
+sudo podman run --shm-size=1gb -it --privileged --name neurodesktop -v ~/neurodesktop-storage:/neurodesktop-storage -e HOST_UID="$(id -u)" -e HOST_GID="$(id -g)" -p 8080:8080 neurodesktop:latest --debug
 # -e CVMFS_DISABLE=true # will disable CVMFS for testing purposes
